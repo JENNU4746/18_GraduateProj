f23f45f33fc058540ffbf9745ad1017df3b53a0f
JENNU4746 <jennu4746@icloud.com>
2018-06-03 22:30:34 +0900

    Added Ingame Scene

---
+++ Assets/Scripts/Ingame/Dialogue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    public Text str;
+    string stringToDisplay;
+    string realString;
+    float timeGone;
+    int count;
+
+	// Use this for initialization
+	void Start () {
+        timeGone = 0; count = 0;
+        realString = "Hello!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while(count<realString.Length)
+        {
+            if (timeGone > 5)
+            {
+                stringToDisplay += realString[count];
+                Debug.Log(stringToDisplay);
+                str.text = stringToDisplay;
+                count++;
+                timeGone = 0;
+            }
+            timeGone += Time.deltaTime;
+        }
+	}
+}
---
+++ Assets/Scripts/Ingame/DialogueMgr.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueMgr : MonoBehaviour {
+
+    private bool isDialogueActive = true;
+    public GameObject DialogueWindow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isDialogueActive == false)
+            {
+                isDialogueActive = true;
+            }
+            else
+            {
+                isDialogueActive = false;
+            }
+        }
+        if (isDialogueActive==false)
+        {
+            DialogueWindow.SetActive(false);
+        }
+        if (isDialogueActive == true)
+        {
+            DialogueWindow.SetActive(true);
+        }
+    }
+}
